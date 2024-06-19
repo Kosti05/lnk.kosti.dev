ee31e6bcac1c792b0788ffc968af6fca4bbd10b2
Kosti05 <62342542+ItsxKosti05@users.noreply.github.com>
2024-06-19 22:48:32 +0200

    initial

---
+++ app.js
@@ -0,0 +1,73 @@
+// index.js
+
+const express = require('express');
+const connection = require('./dbConnection');
+
+const path = require('path');
+const app = express();
+const port = 80;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public', 'index')));
+
+// Dynamic route handling for any /xxx
+app.get('/:code', async (req, res) => {
+    const code = req.params.code;
+    
+    if (code == 'create') {
+        const url = req.query.url;
+        if(!url) {
+            res.status(400);
+            res.sendFile(path.join(__dirname, 'public', 'error', '400.html'));
+            return;
+        }
+        const url_code = generateCode();
+        setURL(url, url_code);
+        res.status(200).send(url_code);
+        return;
+    }
+
+    // Logic to check if the resource is available
+    const url = await getURL(code);
+    if (url) {
+        res.redirect(url);
+    } else {
+        res.redirect('http://localhost');
+    }
+});
+
+
+
+async function getURL(code) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT url FROM links WHERE code = ?', [code], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                results = JSON.parse(JSON.stringify(results));
+                resolve(results[0]?.url);
+            }
+        });
+    });
+}
+
+function setURL(url, code) {
+    connection.query('INSERT INTO links (url, code) VALUES (?, ?)', [url, code], (error, results, fields) => {
+        if (error) throw error;
+    });
+}
+
+function generateCode() {
+    let code = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let counter = 0; counter < 10; counter++) {
+        code += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return code;
+}
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ public/index/script.js
@@ -0,0 +1,85 @@
+var input = document.getElementsByClassName("input")[0];
+var url_input = document.getElementsByClassName("url_input")[0];
+var submit = document.getElementsByClassName("submit")[0];
+
+let forbiddenURL = "The URL must contain http:// or https://";
+
+async function createURL() {
+
+    if (!url_input.value) {
+        alert(forbiddenURL);
+        return;
+    }
+
+    if (!url_input.value.includes("https://")) {
+        if (!url_input.value.includes("http://")) {
+            alert(forbiddenURL);
+            return;
+        }
+    }
+
+    const url = "http://localhost/create?url=" + url_input.value;
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const urlCode = await response.text();
+            console.log(`Shortened URL code: ${urlCode}`);
+            outputURL(urlCode);
+            url_input.value = "";
+        } else {
+            console.error('Failed to create shortened URL', response.status, response.statusText);
+        }
+    } catch (error) {console.error('Error occurred while creating shortened URL', error);}
+}
+
+var output_url = document.getElementsByClassName("output_url")[0];
+
+function outputURL(urlCode) {
+    output_url.value = "https://lnk.kosti.dev/" + urlCode;
+    open();
+}
+
+input.addEventListener("focusin", activate);
+input.addEventListener("focusout", deactivate);
+
+function activate() {
+  document.querySelector(".url_label").classList.add("label_focused");
+  document.querySelector(".url_input").classList.add("input_focused");
+  document.querySelector(".line").classList.add("line_focused");
+  document.querySelector(".submit").classList.add("submit_focused");
+  submit.disabled = false;
+}
+
+function deactivate() {
+  if (url_input.value != "") {
+    return;
+  }
+
+  document.querySelector(".url_label").classList.remove("label_focused");
+  document.querySelector(".url_input").classList.remove("input_focused");
+  document.querySelector(".line").classList.remove("line_focused");
+  document.querySelector(".submit").classList.remove("submit_focused");
+  submit.disabled = true;
+}
+
+//Modal JS
+
+var closeButton = document.getElementsByClassName("close_button")[0];
+var dialog      = document.getElementsByClassName("dialog")[0];
+var copyButton  = document.getElementsByClassName("copy_button")[0];
+
+closeButton.addEventListener("click", close);
+copyButton.addEventListener("click", copy);
+
+
+function close() {
+    dialog.style.visibility='hidden';
+}
+
+function open() {
+    dialog.style.visibility='visible';
+}
+
+function copy() {
+    navigator.clipboard.writeText(output_url.value);
+}
